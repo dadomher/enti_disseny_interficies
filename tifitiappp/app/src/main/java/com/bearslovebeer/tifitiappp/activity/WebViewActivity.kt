78e4f902541ad0c98380d992f3fd86e23b6e2b83
Daniel Domínguez Hernández <dadomher@gmail.com>
2021-01-24 17:30:33 +0100

    clase 11/01/21

--- tifitiappp/app/src/main/java/com/bearslovebeer/tifitiappp/activity/WebViewActivity.kt
+++ tifitiappp/app/src/main/java/com/bearslovebeer/tifitiappp/activity/WebViewActivity.kt
@@ -2,15 +2,29 @@ package com.bearslovebeer.tifitiappp.activity
 
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.webkit.WebResourceRequest
 import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.bearslovebeer.tifitiappp.Constants
 import com.bearslovebeer.tifitiappp.R
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.request.parameter
+import io.ktor.client.request.post
+import io.ktor.client.statement.HttpStatement
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class WebViewActivity : AppCompatActivity() {
 
     private var webView: WebView? = null
+    private val TAG: String = "WebViewActivity"
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +36,7 @@ class WebViewActivity : AppCompatActivity() {
         val fragment = intent.getStringExtra("FRAGMENT")
 
         if(fragment.equals("TWITCH")) {
-            webView!!.loadUrl(loadOAuthUrl())
+            loadOAuthUrl()
         } else if(fragment.equals("NEWS")) {
             val webPage = intent.getStringExtra("WEB_PAGE")
             webView!!.loadUrl(webPage.toString())
@@ -31,15 +45,57 @@ class WebViewActivity : AppCompatActivity() {
 
     }
 
-    private fun loadOAuthUrl(): String {
+    private fun loadOAuthUrl(){
 
-            val uri = Uri.parse("https://id.twitch.tv/oauth2/authorize")
+        val uri = Uri.parse("https://id.twitch.tv/oauth2/authorize")
                 .buildUpon()
                 .appendQueryParameter("client_id", Constants.OAUTH_CLIENT_ID)
                 .appendQueryParameter("redirect_uri", Constants.OAUTH_REDIRECT_URI)
                 .appendQueryParameter("response_type", "code")
-                .appendQueryParameter("scope", listOf("user:edit", "user:read:email").joinToString { " " })
+                .appendQueryParameter("scope", listOf("user:edit", "user:read:email").joinToString(" "))
 
-        return uri.toString()
+        webView!!.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                if(request?.url?.toString()?.startsWith(Constants.OAUTH_REDIRECT_URI) == true) {
+                    // Login success!
+                    Log.i(TAG, "Login succsess with URL: ${request?.url}")
+                    request.url.getQueryParameter("code").let {
+                        Log.i(TAG, "Got authentication CODE$it")
+
+                        webView!!.visibility = View.GONE
+                        //Exchange copde -> acces token
+                        getAccessTokens(it.toString())
+                    } ?: run {
+                        // TODO: Handle error
+                    }
+                }
+                return super.shouldOverrideUrlLoading(view, request)
+            }
+        }
+
+        Log.i(TAG, "Starting login with URL: ${uri.toString()}")
+
+        webView!!.loadUrl(uri.toString())
+    }
+
+    private fun getAccessTokens(authorizationCode: String) {
+        val httpClient = HttpClient(OkHttp)
+
+        // Assign to Activity Scope
+        lifecycleScope.launch {
+
+            //Change to Background Thread
+            withContext(Dispatchers.IO) {
+
+                val response: String = httpClient.post<String>("https://id.twitch.tv/oauth2/token") {
+                    parameter("client_id", Constants.OAUTH_CLIENT_ID)
+                    parameter("client_secret", Constants.OAUTH_CLIENT_SECRET)
+                    parameter("code", authorizationCode)
+                    parameter("grant_type", "authorization_code")
+                    parameter("redirect_uri", Constants.OAUTH_REDIRECT_URI)
+                }
+                Log.i(TAG, "Got response from Twitch: $response")
+            }
+        }
     }
 }
---
+++ tifitiappp/app/src/main/java/com/bearslovebeer/tifitiappp/models/OAuthTokensResponse.kt
@@ -0,0 +1,11 @@
+package com.bearslovebeer.tifitiappp.models
+
+import kotlinx.serialization.SerialName
+
+data class OAuthTokensResponse (
+    @SerialName("access_token") val accessToken: String,
+    @SerialName("refresh_token")val refreshToken: String? = null,
+    @SerialName("expires_in")val expiresIn: Int? = null,
+    val scopes: List<String>? = null,
+    @SerialName("token_type")val tokenType: String? = null
+)
